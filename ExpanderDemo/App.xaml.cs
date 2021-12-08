bc96c3a9903e493bf1879967e4952b12fbe111db
eugeniiya <evgenia.fonina.2013@yandex.ru>
2021-12-08 22:30:10 +0300

    Добавьте файлы проекта.

---
+++ ExpanderDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit;
+namespace ExpanderDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new string[] { "Expander_Experimental" });
+            MainPage = new MainPage();
+        }
+    }
+}
